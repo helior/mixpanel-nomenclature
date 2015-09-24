550c443f48c18d58cf790171f22b27d7d327d0ee
Helior Colorado <me@helior.info>
2015-09-23 23:04:37 -0700

    More tests

--- test/nomenclature.js
+++ test/nomenclature.js
@@ -4,19 +4,31 @@ var mixpanelApi = require('./mock-mixpanel.js');
 var nomenclature = require('../dist/nomenclature');
 var MixpanelNomenclature = require('..');
 
-test('Wrapping original API works as expected.', function (t) {
+test('Contstructor and property accessors work as expected.', function (t) {
   var spec = {};
-
   t.doesNotThrow(function() {new nomenclature(mixpanelApi, spec)});
-  var goodNomen = new nomenclature(mixpanelApi, spec);
 
-  t.equal(goodNomen.spec, spec);
-  t.equal(goodNomen.api, mixpanelApi);
+  var instance = new nomenclature(mixpanelApi, spec);
+
+  t.equal(instance.spec, spec);
+  t.equal(instance.api, mixpanelApi);
   t.end();
 });
 
-test('Expect behavior of a bad nomenclature', function(t) {
-  var badSpec = null;
-  t.throws(function () {new nomenclature(mixpanelApi, badSpec)});
+test('Negative test for constructor', function(t) {
+  var spec = null;
+  t.throws(function () {new nomenclature(mixpanelApi, spec)});
+  t.end();
+});
+
+test('Helper methods', function(t) {
+  spec = {'foo' : 'bar'};
+  var instance = new nomenclature(mixpanelApi, spec);
+  t.ok(instance.specDefinitionExists('foo'), 'foo should exist in spec');
+  t.notOk(instance.specDefinitionExists('baz'), 'baz should not exist in spec');
+
+  t.equal(instance.getSpecItem('foo'), 'bar', 'foo should equal bar');
+  t.notEqual(instance.getSpecItem('foo', 'baz', 'foo should not equal baz'));
+  t.equal(instance.getSpecItem('fakeProperty'), undefined, 'fakeProperty should be undefined');
   t.end();
 });
