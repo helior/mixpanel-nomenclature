afd9419bea807e26e40d810f28155110ebb60b8e
Helior Colorado <me@helior.info>
2015-10-02 17:26:08 -0700

    Cleaned-up tests

--- test/nomenclature.js
+++ test/nomenclature.js
@@ -4,31 +4,50 @@ var mixpanelApi = require('./mock-mixpanel.js');
 var nomenclature = require('../dist/nomenclature');
 var MixpanelNomenclature = require('..');
 
-test('Contstructor and property accessors work as expected.', function (t) {
-  var spec = {};
-  t.doesNotThrow(function() {new nomenclature(mixpanelApi, spec)});
+test('Nomenclature.constructor()', function (t) {
+  t.throws(function () {new nomenclature(mixpanelApi, null)});
+  t.throws(function () {new nomenclature(mixpanelApi, 123)});
+  t.throws(function () {new nomenclature(mixpanelApi, 'will not work')});
+  t.doesNotThrow(function() {new nomenclature(mixpanelApi, {})});
 
+  t.end();
+});
+
+test('Nomenclature.get', function (t) {
   var instance = new nomenclature(mixpanelApi, spec);
-
   t.equal(instance.spec, spec);
   t.equal(instance.api, mixpanelApi);
   t.end();
 });
 
-test('Negative test for constructor', function(t) {
+test('Nomenclature.validateSpec()', function (t) {
   var spec = null;
-  t.throws(function () {new nomenclature(mixpanelApi, spec)});
+  var instance = new nomenclature(mixpanelApi, {});
+
+  t.throws(function () {instance.validateSpec(null)});
+  t.throws(function () {instance.validateSpec(123)});
+  t.doesNotThrow(function () {instance.validateSpec({})});
+  t.doesNotThrow(function () {instance.validateSpec({'foo': 'bar'})});
   t.end();
 });
 
-test('Helper methods', function(t) {
+test('Nomenclature.specDefinitionExists()', function (t) {
   spec = {'foo' : 'bar'};
   var instance = new nomenclature(mixpanelApi, spec);
+
   t.ok(instance.specDefinitionExists('foo'), 'foo should exist in spec');
   t.notOk(instance.specDefinitionExists('baz'), 'baz should not exist in spec');
+  t.end();
+});
+
+test('Nomenclature.getSpecItem()', function (t) {
+  spec = {'foo' : 'bar'};
+  var instance = new nomenclature(mixpanelApi, spec);
 
   t.equal(instance.getSpecItem('foo'), 'bar', 'foo should equal bar');
   t.notEqual(instance.getSpecItem('foo', 'baz', 'foo should not equal baz'));
   t.equal(instance.getSpecItem('fakeProperty'), undefined, 'fakeProperty should be undefined');
   t.end();
+
+
 });
